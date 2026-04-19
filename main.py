1b619c09ab50a53d14bc19b75f087112a46c7da1
adamagents <mr.shernosk@gmail.com>
2026-04-18 21:20:38 -0700

    stockanalysis, find tickers, make api calls in parallel for more signals

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from sources.yahoo_finance.setup import YahooFinance
 from sources.yahoo_finance.utils.constants import BASE_API
 
-from sources.stockanalysis.setup import StockAnalysis
+from sources.stockanalysis.ingestion import Ingestion
+from sources.stockanalysis.analysis import Analysis
 
 if __name__ == "__main__":
-    finance = StockAnalysis()
-    response= finance.get_raw_text()
+    finance_ingestion = Ingestion()
+    response= finance_ingestion.get_financial_data()
     
+    finance_analysis=Analysis()
+    finance_analysis.create()
--- sources/stockanalysis/ingestion.py
+++ sources/stockanalysis/ingestion.py
@@ -2,8 +2,8 @@ import requests
 from sources.stockanalysis.utils.constants import ETF_BASE_URL
 import json 
 import re 
-class StockAnalysis:
 
+class Ingestion:
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update({
@@ -11,7 +11,13 @@ class StockAnalysis:
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         })
      
-    def get_raw_text(self):
+    @classmethod
+    def create(self):
+        self.get_financial_data()
+        self.parser()
+        self.save_json()
+
+    def get_financial_data(self):
         response=self.session.get(ETF_BASE_URL)
         if response.ok:
             self.parser(response.text)
@@ -22,18 +28,14 @@ class StockAnalysis:
             print("count not found")
             return
         
-        # extract everything from the data array onwards
         data_start = text.find("data:[", idx)
         if data_start == -1:
             print("data array not found")
             return
         
-        # move past "data:" to get to the raw array
         array_start = data_start + len("data:")
         raw = text[array_start:]
         
-        # find the closing bracket of the array
-        # count brackets to find the matching close
         depth = 0
         end_idx = 0
         for i, char in enumerate(raw):
@@ -47,11 +49,9 @@ class StockAnalysis:
         
         raw = raw[:end_idx]
         
-        # convert JS to valid JSON
-        # convert JS to valid JSON
         clean = re.sub(r'(\b\w+\b)(?=\s*:)', r'"\1"', raw)
-        clean = re.sub(r':\.(\d+)', r':0.\1', clean)      # :.82 → :0.82
-        clean = re.sub(r'-\.(\d+)', r'-0.\1', clean)       # -.41 → -0.41
+        clean = re.sub(r':\.(\d+)', r':0.\1', clean)     
+        clean = re.sub(r'-\.(\d+)', r'-0.\1', clean)      
 
         try:
             data = json.loads(clean)
@@ -62,7 +62,7 @@ class StockAnalysis:
             print(clean[e.pos-50:e.pos+50])
 
     def save_json(self,text):
-        with open("./sources/stockanalysis/data.json","w") as file:
+        with open("./sources/stockanalysis/results/ingested.json","w") as file:
             json.dump(text, file, indent=2)
 
         
